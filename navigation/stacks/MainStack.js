0053b2beedf9f0a815d85d03f054b6c5552f8ef5
Mohan Rathod <mohan.rathod2008@gmail.com>
2022-03-07 00:28:02 +0530

    Ranking page changes
    Ranking page changes

--- navigation/stacks/MainStack.js
+++ navigation/stacks/MainStack.js
@@ -13,6 +13,8 @@ import {
   RateUs,
   RateUsnavigationOptions,
 } from "../../screens/FeedbackScreens";
+import Leadboard from "../../components/SettingsComponents/Leadboard";
+import Ranking from "../../components/SettingsComponents/Ranking";
 
 const defaultOptions = {
   headerStyle: {
@@ -43,5 +45,7 @@ export default MainStack = () => (
     <MainStackNavigator.Screen name="Questions" component={QuestionsScreen} />
     <MainStackNavigator.Screen name="Instructions" component={Instructions} />
     <MainStackNavigator.Screen name="Result" component={ResultScreen} />
+    <MainStackNavigator.Screen name="Ranking" component={Ranking} />
+    <MainStackNavigator.Screen name="Leadboard" component={Leadboard} />
   </MainStackNavigator.Navigator>
 );
