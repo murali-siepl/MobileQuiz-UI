a29cad64fb7d39d328fd03e8b4cda5cc7f1ab456
Mohan Rathod <mohan.rathod2008@gmail.com>
2022-06-30 23:27:34 +0530

    feedback api fix
    feedback api fix

--- components/shared/SuccessModal.js
+++ components/shared/SuccessModal.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Alert } from "react-native";
 
-const SuccessModal = () => {
+const SuccessModal = (message) => {
   Alert.alert("Success", message, [{ text: "ok", style: "cancel" }]);
 };
 
